23047e7b95dc87f645d027b67ca1008b75731d90
Christian Amaral <darthcas@gmail.com>
2015-03-10 00:22:22 -0300

    auto configuração do db

--- src/crawler/archive.js
+++ src/crawler/archive.js
@@ -2,7 +2,7 @@ module.exports = {
   save(ads) {
     require('./../db')().then(
       db =>
-        db.r.table('ads').insert(ads, {conflict: 'update'})
+        db.r.table('ad').insert(ads, {conflict: 'update'})
           .run(db.conn).finally(() => db.conn.close()),
       err => console.log(err)
     );
--- src/db.js
+++ src/db.js
@@ -1,8 +1,21 @@
 var Promise = require('bluebird');
 var r = require('rethinkdb');
+var noop = require('lodash').noop;
 
 module.exports = () => new Promise((resolve, reject) =>
-    r.connect({host: 'localhost', port: 28015, db: 'pmy'})
-      .then(conn => resolve({r: r, conn: conn}), err => reject(err))
+    r.connect({host: 'localhost', port: 28015})
+      .then(conn =>
+        (db =>
+          db.r.dbCreate('pmy')
+            .run(db.conn)
+            .catch(noop)
+            .finally(() => (
+              db.conn.use('pmy'),
+                db.r.tableCreate('ad').run(db.conn)
+                  .finally(() => resolve(db))
+                  .catch(noop)
+            )))({r: r, conn: conn}),
+        err => reject(err)
+    )
 );
 
